7d88cbba3aac6220550938568d332c5078c096b1
saluo3219-oss <saluo3219@gmail.com>
2026-03-30 08:44:31 +0900

    Update board.cmake

--- config/boards/arm/seeeduino_xiao_ble/board.cmake
+++ config/boards/arm/seeeduino_xiao_ble/board.cmake
@@ -1,3 +1,2 @@
-config/boards/arm/seeeduino_xiao_ble/board.cmake
-
-include(${ZEPHYR_BASE}/boards/common/nrf52.board.cmake)
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
